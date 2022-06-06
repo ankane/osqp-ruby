a898c6631fee8e74fedbffa1869c182c3129b433
Andrew Kane <andrew@ankane.org>
2022-06-06 00:29:00 -0700

    Fixed CI

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,10 +5,10 @@ require "minitest/pride"
 require "numo/narray"
 
 class Minitest::Test
-  def assert_elements_in_delta(expected, actual)
+  def assert_elements_in_delta(expected, actual, delta = 0.001)
     assert_equal expected.size, actual.size
     expected.zip(actual) do |exp, act|
-      assert_in_delta exp, act
+      assert_in_delta exp, act, delta
     end
   end
 end
